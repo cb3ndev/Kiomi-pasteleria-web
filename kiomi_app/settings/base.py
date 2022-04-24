0bf8b3cea53a1a07ae03e3003cd73f45df85767e
Cristian Benites <130351@unsaac.edu.pe>
2022-04-24 11:27:36 -0500

    cambio de configuracion deployment heroku

--- kiomi_app/settings/base.py
+++ kiomi_app/settings/base.py
@@ -54,6 +54,8 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
     # cloudinary para imagenes
     'cloudinary',
+    # deployment heroku:
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
@@ -64,6 +66,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # deployment heroku:
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'kiomi_app.urls'
--- kiomi_app/settings/production.py
+++ kiomi_app/settings/production.py
@@ -1,10 +1,10 @@
 from .base import *
-
+import dj_database_url
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['kiomi-test-v1.herokuapp.com']
+ALLOWED_HOSTS = ['kiomi-pasteleria.herokuapp.com']
 
 # host allowed
 CORS_ALLOWED_ORIGINS = [
@@ -21,13 +21,16 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'd9198mrdp4nqu3',
-				'USER': 'knodbuhkvablcf',
-				'PASSWORD': 'f7bd54050a9b1a5701ffc32db4362e0c8099096e4c85e4c07b812e8bd7833e69',
-				'HOST': 'ec2-3-222-235-188.compute-1.amazonaws.com',
-				'PORT': 5432,
+        'USER': 'knodbuhkvablcf',
+        'PASSWORD': 'f7bd54050a9b1a5701ffc32db4362e0c8099096e4c85e4c07b812e8bd7833e69',
+        'HOST': 'ec2-3-222-235-188.compute-1.amazonaws.com',
+        'PORT': 5432,
     }
 }
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 STATICFILES_DIRS = (BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
